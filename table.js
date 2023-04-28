cf43435e34592fd19e705f7c98962194f7ca5a44
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-04-28 15:47:13 -0400

    deleted nav_bar.html, centered the entity table

--- table.js
+++ table.js
@@ -61,6 +61,8 @@ function createTable() {
                 init.innerText = entities[i][3];
 
                 del.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16" style="cursor: pointer" onclick="deleteRow(this)"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/></svg>';
+                newRow.style.textAlign = 'center';
+                newRow.style.color = '#090707';
                 // "<button class='button_delete' onclick = 'deleteRow(this)''>X</button>";
         }
 }
